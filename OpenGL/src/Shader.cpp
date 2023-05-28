48ba64b481385ab11b75fbc4db178b7e406dfec5
l3an <zjq990822@163.com>
2023-05-28 15:30:02 +0800

    27.How to make your UNIFORMS FASTER in OpenGL.

--- OpenGL/src/Shader.cpp
+++ OpenGL/src/Shader.cpp
@@ -44,12 +44,22 @@ void Shader::SetUniform1f(const std::string & name, float value)
 	GLCall(glUniform1f(GetUniformLocation(name), value));
 }
 
+void Shader::SetUniform2f(const std::string & name, float v0, float v1)
+{
+	GLCall(glUniform2f(GetUniformLocation(name), v0, v1));
+}
+
+void Shader::SetUniform3f(const std::string & name, float v0, float v1, float v2)
+{
+	GLCall(glUniform3f(GetUniformLocation(name), v0, v1, v2));
+}
+
 void Shader::SetUniform4f(const std::string & name, float v0, float v1, float v2, float v3)
 {
 	GLCall(glUniform4f(GetUniformLocation(name), v0, v1, v2, v3));
 }
 
-int Shader::GetUniformLocation(const std::string& name)
+int Shader::GetUniformLocation(const std::string& name) const
 {
 	if (m_UniformLocationCache.find(name) != m_UniformLocationCache.end())
 		return m_UniformLocationCache[name];
--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -23,15 +23,18 @@ public:
 	// Set uniforms
 	void SetUniform1i(const std::string& name, int value);
 	void SetUniform1f(const std::string& name, float value);
+	void SetUniform2f(const std::string& name, float v0, float v1);
+	void SetUniform3f(const std::string& name, float v0, float v1, float v2);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+
 	void SetUniformMat4f(const std::string& name, glm::mat4& matrix);
 private:
 	std::string m_FilePath;
 	unsigned int m_RendererID;
 	// caching for uniforms
-	std::unordered_map<std::string, int> m_UniformLocationCache;
+	mutable std::unordered_map<std::string, int> m_UniformLocationCache;
 
-	int GetUniformLocation(const std::string& name);
+	int GetUniformLocation(const std::string& name) const;
 	shaderProgramSource ParseShader(const std::string filepath);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
